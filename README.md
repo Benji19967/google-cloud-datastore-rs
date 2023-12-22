c44c62160a5531e2e77a4d3719c4322c54b473f2
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-12-21 21:43:24 -0500

    Update README with gcloud auth and add updates go gRPC datastore client

--- README.md
+++ README.md
@@ -3,6 +3,35 @@
 There are clients available for Google Cloud Storage, such as https://github.com/yoshidan/google-cloud-rust/tree/main
 but none for Datastore. Hence, I will try to write one for Datastore.
 
+## Google Cloud
+
+### Auth
+
+`gcloud` needs to be installed:
+
+```bash
+brew install --cask google-cloud-sdk
+```
+
+Authenticate: 
+
+```bash
+gcloud auth login
+```
+
+Set the project id:
+
+```bash
+gcloud config set project <project_id>
+```
+
+You're good to go! You can run commands such as:
+
+```bash
+gcloud storage buckets list
+```
+
+
 ## Proto
 
 To be able to use `tonic`: https://github.com/hyperium/tonic
@@ -20,6 +49,7 @@ git submodule update --remote
 ## Resources
 
 ### gRPC
+
 - https://googleapis.github.io/HowToRPC.html
   - How to call Google RPC APIs
 - https://github.com/googleapis/googleapis/blob/master/google/datastore/v1/datastore.proto
@@ -28,3 +58,7 @@ git submodule update --remote
   - Rust gRPC crate
 - https://github.com/tokio-rs/prost
   - A Protocol Buffers implementation for Rust
+
+### Datastore Client Example using gRPC
+
+- https://github.com/i110/google-cloud-rs/blob/c826356348972c859886f9b439d183a50dcf9047/google-cloud/src/datastore/client.rs
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod api {
     tonic::include_proto!("google.datastore.v1");
 }
+use api::datastore_client::DatastoreClient;
 use api::key::path_element::IdType;
 use api::key::PathElement;
 use api::read_options::ConsistencyType;
@@ -9,6 +10,10 @@ use api::Key;
 use api::LookupRequest;
 use api::PartitionId;
 use api::ReadOptions;
+use std::env;
+use std::fs::File;
+use tonic::transport::{Certificate, Channel, ClientTlsConfig};
+use tonic::IntoRequest;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,7 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(LookupRequest {
         database_id: "".into(),
         // TODO: Add project_id
-        project_id: "".into(),
+        project_id: "bxdx-dev".into(),
         read_options: Some(ReadOptions {
             consistency_type: Some(ConsistencyType::ReadConsistency(
                 ReadConsistency::Strong.into(),
@@ -29,7 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     database_id: "".into(),
                     // TODO: Add namespace_id and project
                     namespace_id: "default".into(),
-                    project_id: "".into(),
+                    project_id: "bxdx-dev".into(),
                 })
             },
             path: vec![PathElement {
@@ -40,10 +45,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .into(),
     });
 
-    // TODO: Send the request
-    // let response = client.say_hello(request).await?;
+    let path = env::var("GOOGLE_APPLICATION_CREDENTIALS")?;
+    let file = File::open(path)?;
+    let creds = serde_json::from_reader(file)?;
 
-    // println!("RESPONSE={:?}", response);
+    const DOMAIN_NAME: &'static str = "datastore.googleapis.com";
+    const ENDPOINT: &'static str = "https://datastore.googleapis.com";
+    const TLS_CERTS: &[u8] = include_bytes!("/etc/ssl/cert.pem");
+    let tls_config = ClientTlsConfig::new()
+        .ca_certificate(Certificate::from_pem(TLS_CERTS))
+        .domain_name(DOMAIN_NAME);
+    let channel = Channel::from_static(ENDPOINT)
+        .tls_config(tls_config)?
+        .connect()
+        .await?;
+    let mut client = DatastoreClient::new(channel);
+    let mut request = request.into_request();
+    let metadata = request.metadata_mut();
+    metadata.insert("authorization", token);
+    let response = client.lookup(request).await.unwrap();
+    let response_message = response.into_inner();
+
+    println!("{:?}", response_message);
 
     Ok(())
 }
